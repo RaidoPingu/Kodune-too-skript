eeeda5c74cbf6cc1e5a5bb72fc3de587f2233aa3
RaidoPingu <suurmets@protonmail.com>
2025-09-20 12:43:16 +0300

    Vahepealne

--- skript.py
+++ skript.py
@@ -1,1 +1,8 @@
 import random
+from datetime import datetime
+
+time = datetime.now()
+filename = 'andmed.txt'
+
+with open(filename, 'w', encoding='utf-8') as f:
+    f.write(f'Kuupäev: {str(time)}')
