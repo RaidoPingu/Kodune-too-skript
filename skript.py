89dfabaaf7f716dadd0ab5862d121d374697f121
RaidoPingu <suurmets@protonmail.com>
2025-09-20 18:06:34 +0300

    järgmine osa valmis

--- skript.py
+++ skript.py
@@ -8,7 +8,7 @@ from datetime import datetime
 time = datetime.now() # Kutsume välja 
 vormindatud =time.strftime('%d.%m.%Y %H:%M:%S') # Vormindmae aja ära, et oleks nagu vaja 
 filename = 'andmed.txt' # Genereeritava faili nimi
-
+arvud =[] # Tühi list kuhu hiljem korjame arvud.
 # Siit hakkab faili genereerimise ning kuupäeva ja juhuslike arvude salvestamise kood.
 
 with open(filename, 'w', encoding='utf-8') as f: # Avame faili kirjutamiseks
@@ -22,5 +22,15 @@ with open(filename, 'w', encoding='utf-8') as f: # Avame faili kirjutamiseks
 
 # Siit alates loeme loodud failist arvud sisse, töötleb ja kuvab neid
 
+    # Loeme faili sisse ja puhastame välja ainult arvud listi
 
+with open(filename, 'r', encoding='utf-8' ) as f:
+    contents = f.readlines()[1:] # Loeme faili sisu muutujasse, alates teisest reast
+    for parts in contents: # Loop elementide lahti võtmiseks
+        parts = parts.split() # Kaotame ära tühikud
+        for part in parts: # teine loop mis võtab 1 elemendi
+            if part.isdigit(): # Kontrollime kas on tegu arvuga
+                arvud.append(int(part)) # Kui on arv lisame loodud tühja listi. 
+
+    #Võtame selle listi ja teem maagiat.
 
