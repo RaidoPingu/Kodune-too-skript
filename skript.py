f583e3d8f616182d797c35e72bed951c5e846ae9
RaidoPingu <suurmets@protonmail.com>
2025-09-20 15:02:32 +0300

    Esimene osa tehtud

--- skript.py
+++ skript.py
@@ -1,8 +1,26 @@
+"""
+
+"""
+
 import random
 from datetime import datetime
 
-time = datetime.now()
-filename = 'andmed.txt'
+time = datetime.now() # Kutsume välja 
+vormindatud =time.strftime('%d.%m.%Y %H:%M:%S') # Vormindmae aja ära, et oleks nagu vaja 
+filename = 'andmed.txt' # Genereeritava faili nimi
 
-with open(filename, 'w', encoding='utf-8') as f:
-    f.write(f'Kuupäev: {str(time)}')
+# Siit hakkab faili genereerimise ning kuupäeva ja juhuslike arvude salvestamise kood.
+
+with open(filename, 'w', encoding='utf-8') as f: # Avame faili kirjutamiseks
+    f.write(f'Kuupäev: {vormindatud} \n') # Kirjutame kohe kuupäeva faili
+    f.write('Arvud: ') # Alustame uuelt realt sõnadega arvud
+    count = 0 # Teeme While loopi jaoks counteri
+    while count < 20: # While loop juhuslike arvude genereerimiseks
+        nr = random.randint(1, 101) # Genereerime juhusliku arvu
+        f.write(f' {nr}') # Kirjutame selle arvu faili
+        count += 1 # Lisame +1 counterisse.
+
+# Siit alates loeme loodud failist arvud sisse, töötleb ja kuvab neid
+
+
+
